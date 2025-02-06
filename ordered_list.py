492a517a8f28b434b4aeaf6b5b61ee676a0c6eec
pdcm2302-uol <pdcm2302@leeds.ac.uk>
2025-02-06 16:43:54 +0000

    Add [ordered_list.py or search_list.py] module

--- ordered_list.py
+++ ordered_list.py
@@ -1,3 +1,6 @@
-def get_ordered_list():
-
-  
+def get_ordered_list(lst=None):
+    if lst is None:
+        user_input_input('Enter a list of integers (comma-separated):')
+        lst = [int(num.strp()) for num in user_input.split(',')]
+    return sorted(lst)
+    
